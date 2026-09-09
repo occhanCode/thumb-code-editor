a28730bbaa6460aedc1a2d95af95bd1cf5aaf0ca
おっちゃん <62776507+occhanCode@users.noreply.github.com>
2026-09-09 17:03:23 +0900

    Fix eye position values in overlay.js

--- occhan_vtuber_web/overlay.js
+++ occhan_vtuber_web/overlay.js
@@ -221,14 +221,14 @@ const OVT = (() => {
 
   // 元画像上の目の位置に合わせた値
   const eyeLeft = createBlinkEye({
-    cx: 135,
+    cx: 134,
     cy: 264,
     rx: 29,
     ry: 22
   });
   
   const eyeRight = createBlinkEye({
-    cx: 238,
+    cx: 237,
     cy: 264,
     rx: 29,
     ry: 22
